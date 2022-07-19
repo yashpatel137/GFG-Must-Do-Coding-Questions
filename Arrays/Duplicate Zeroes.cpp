773c8707dd5f9aa46e74d0c0f5edc1786004116f
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-19 16:18:48 +0530

    Update Duplicate Zeroes.cpp

--- Arrays/Duplicate Zeroes.cpp
+++ Arrays/Duplicate Zeroes.cpp
@@ -16,3 +16,25 @@ public:
         }
     }
 };
+
+
+//Method 2
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int s=arr.size(), i=s-1;
+        while(i>=0)
+        {
+            if(arr[i] != 0)
+            {
+                
+            }
+            else
+            {
+                arr.insert(arr.begin()+i,0);
+                arr.pop_back();
+            }
+            --i;
+        }
+    }
+};
