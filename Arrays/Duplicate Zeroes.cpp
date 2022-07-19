fd3d1489110897f357cbb0604c66ef6b366583a1
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-19 16:19:22 +0530

    Update Duplicate Zeroes.cpp

--- Arrays/Duplicate Zeroes.cpp
+++ Arrays/Duplicate Zeroes.cpp
@@ -25,11 +25,7 @@ public:
         int s=arr.size(), i=s-1;
         while(i>=0)
         {
-            if(arr[i] != 0)
-            {
-                
-            }
-            else
+            if(arr[i] == 0)
             {
                 arr.insert(arr.begin()+i,0);
                 arr.pop_back();
