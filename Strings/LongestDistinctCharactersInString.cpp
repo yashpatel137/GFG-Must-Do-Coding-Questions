4f943a80063b43670090160f06affefe3d488b6c
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-20 16:40:42 +0530

    Update LongestDistinctCharactersInString.cpp

--- Strings/LongestDistinctCharactersInString.cpp
+++ Strings/LongestDistinctCharactersInString.cpp
@@ -1,3 +1,44 @@
+//Method 1
+class Solution{
+  public:
+    string common(string str1, string str2)
+    {
+        string ans;
+        int n=str1.length();
+        int m=str2.length();
+        for(int i=0,j=0;i<n && j<m;i++,j++)
+        {
+            if(str1[i]!=str2[j])
+            {
+                break;
+            }
+            ans.push_back(str1[i]);
+        }
+        return ans;
+    }
+    string longestCommonPrefix (string arr[], int N)
+    {
+        string prefix=arr[0];
+        string negetive="-1";
+        for(int i=1;i<N;i++)
+        {
+            prefix=common(prefix,arr[i]);
+        }
+        if(prefix.length())
+        {
+            return prefix;
+        }
+        else
+        {
+            return negetive;
+        }
+    }
+};
+
+
+//Method 2
+
+
 // { Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
