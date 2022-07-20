bc80527ef7887c736aa2dee1c87f8bd992858969
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-20 16:22:06 +0530

    Update RomanNumberToInteger.cpp

--- Strings/RomanNumberToInteger.cpp
+++ Strings/RomanNumberToInteger.cpp
@@ -1,28 +1,40 @@
-// { Driver Code Starts
-// Initial template for C++
-
-// Program to convert Roman Numerals to Numbers
-#include <bits/stdc++.h>
-using namespace std;
-
-// Returns decimal value of roman numaral
-int romanToDecimal(string &);
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        cout << romanToDecimal(s) << endl;
+//Method 1
+class Solution {
+  public:
+    int romanToDecimal(string &str) {
+        unordered_map<char,int> mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+        int ans=0;
+        int n=str.length();
+        for(int i=0;i<n;i++)
+        {
+            if(i<n-1)
+            {
+                if(mp[str[i]]>=mp[str[i+1]])
+                {
+                    ans+=mp[str[i]];
+                }
+                else
+                {
+                    ans-=mp[str[i]];
+                }
+            }
+            else
+            {
+                ans+=mp[str[i]];
+            }
+        }
+        return ans;
     }
-}// } Driver Code Ends
+};
 
-
-// User fuunction teemplate for C++
-
-// str given roman number string
-// Returns decimal value of roman numaral
+//Method 2
 int symbolValue(char c)
 {
     if(c == 'I')
