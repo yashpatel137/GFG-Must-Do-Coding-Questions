fe324ca5642414a7c61cf3fd6a3b076c8f0953ec
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-24 11:04:46 +0530

    Update CheckForBST.cpp

--- Trees/CheckForBST.cpp
+++ Trees/CheckForBST.cpp
@@ -1,3 +1,29 @@
+//Method 1
+class Solution
+{
+    public:
+    bool solve(Node *root,int min, int max)
+    {
+        if(!root)
+        {
+            return true;
+        }
+        if(root->data<min || root->data>max)
+        {
+            return false;
+        }
+        return solve(root->left,min,root->data) && solve(root->right,root->data,max);
+    }
+    //Function to check whether a Binary Tree is BST or not.
+    bool isBST(Node* root) 
+    {
+        return solve(root,INT_MIN,INT_MAX);
+    }
+};
+
+
+//Method 2
+
 // { Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
