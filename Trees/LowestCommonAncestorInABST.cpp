61169c8aec343c9ec7b4e205116fc98e572c819c
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-24 12:17:13 +0530

    Update LowestCommonAncestorInABST.cpp

--- Trees/LowestCommonAncestorInABST.cpp
+++ Trees/LowestCommonAncestorInABST.cpp
@@ -1,3 +1,22 @@
+//Method 1
+Node* LCA(Node *root, int n1, int n2)
+{
+    if(n1<root->data and n2<root->data)
+    {
+        return LCA(root->left,n1,n2);
+    }
+    else if(n1>root->data and n2>root->data)
+    {
+        return LCA(root->right,n1,n2);
+    }
+    else
+    {
+        return root;
+    }
+}
+
+//Method 2
+
 // { Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
