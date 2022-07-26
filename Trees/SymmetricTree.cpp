80aafe14cb88b8d0ed175dae466076e99a073031
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-26 11:39:13 +0530

    Update SymmetricTree.cpp

--- Trees/SymmetricTree.cpp
+++ Trees/SymmetricTree.cpp
@@ -1,4 +1,24 @@
-// { Driver Code Starts
+//Method 1
+class Solution {
+public:
+    bool solve(TreeNode *r1, TreeNode *r2)
+    {
+        if(!r1 and !r2)
+        {
+            return true;
+        }
+        if(r1 and r2 and r1->val==r2->val)
+        {
+            return solve(r1->left,r2->right) and solve(r1->right,r2->left);
+        }
+        return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        return solve(root,root);
+    }
+};
+
+//Method 2
 #include<bits/stdc++.h>
 using namespace std;
 struct Node {
