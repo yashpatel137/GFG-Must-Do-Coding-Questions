15a5b273db5487c8932fb952e1e81e254303f93b
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-26 11:58:26 +0530

    Update MaximumPathSumBetweenTwoLeafNodes.cpp

--- Trees/MaximumPathSumBetweenTwoLeafNodes.cpp
+++ Trees/MaximumPathSumBetweenTwoLeafNodes.cpp
@@ -1,4 +1,26 @@
-// { Driver Code Starts
+//Method 1
+class Solution {
+public:
+    int solve(TreeNode *root,int &ans)
+    {
+        if(!root)
+        {
+            return 0;
+        }
+        int l=max(0,solve(root->left,ans));
+        int r=max(0,solve(root->right,ans));
+        ans=max(ans,l+r+root->val);
+        return max(l,r)+root->val;
+    }
+    int maxPathSum(TreeNode* root) {
+        int ans=INT_MIN;
+        solve(root,ans);
+        return ans;
+    }
+};
+
+
+//Method 2
 #include <bits/stdc++.h>
 using namespace std;
 
