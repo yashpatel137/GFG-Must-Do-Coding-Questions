7f4492cabb9f3befd2637960f539f666ccba0ed9
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-29 11:28:30 +0530

    Update DFSOfGraph.cpp

--- Graph/DFSOfGraph.cpp
+++ Graph/DFSOfGraph.cpp
@@ -1,4 +1,30 @@
-// { Driver Code Starts
+//Method 1
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> ans;
+    vector<int> dfs(vector<int> adj[],int r,bool visited[])
+    {
+        visited[r]=true;
+        ans.push_back(r);
+        for(auto x:adj[r])
+        {
+            if(visited[x]==false)
+            {
+                dfs(adj,x,visited);
+            }
+            
+        }
+        return ans;
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        bool visited[V+1]={false};
+        return dfs(adj,0,visited);
+    }
+};
+
+
+//Method 2
 #include<bits/stdc++.h>
 using namespace std;
 
