0214cf7a73a06e3e0d073deabe41bba36a026066
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-29 11:40:31 +0530

    Update BFSOfGraph.cpp

--- Graph/BFSOfGraph.cpp
+++ Graph/BFSOfGraph.cpp
@@ -1,4 +1,33 @@
-// { Driver Code Starts
+//Method 1
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> ans;
+        vector<bool> visit(V);
+        queue<int> q;
+        q.push(0);
+        while(!q.empty())
+        {
+            int temp=q.front();
+            q.pop();
+            visit[temp]=true;
+            ans.push_back(temp);
+            for(auto x:adj[temp])
+            {
+                if(visit[x]==false)
+                {
+                    q.push(x);
+                    visit[x]=true;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//Method 2
+
 #include<bits/stdc++.h>
 using namespace std;
 
