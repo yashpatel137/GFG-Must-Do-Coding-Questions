ae27e64ba9f1f10ac2a537027c7d890ce40a6045
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-31 16:00:01 +0530

    Update TopologicalSort.cpp

--- Graph/TopologicalSort.cpp
+++ Graph/TopologicalSort.cpp
@@ -1,4 +1,46 @@
-// { Driver Code Starts
+//Method 1
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int> ans(V,0),ind(V,0);
+	    for(int i=0;i<V;i++)
+	    {
+	        for(auto it:adj[i])
+	        {
+	            ind[it]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i=0;i<V;i++)
+	    {
+	        if(ind[i]==0)
+	        {
+	            q.push(i);
+	        }
+	    }
+	    int indg=0;
+	    while(!q.empty())
+	    {
+	        int temp=q.front();
+	        q.pop();
+	        ans[indg++]=temp;
+	        for(auto it:adj[temp])
+	        {
+	            ind[it]--;
+	            if(ind[it]==0)
+	            {
+	                q.push(it);
+	            }
+	        }
+	    }
+	    return ans;
+	}
+};
+
+//Method 2
 #include <bits/stdc++.h>
 using namespace std;
 
