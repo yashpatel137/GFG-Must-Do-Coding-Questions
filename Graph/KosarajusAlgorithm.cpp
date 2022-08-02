fed5a8e65e2c0f02bbe69bd2fc93a7dbd6580ae9
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-08-02 15:30:55 +0530

    Update KosarajusAlgorithm.cpp

--- Graph/KosarajusAlgorithm.cpp
+++ Graph/KosarajusAlgorithm.cpp
@@ -1,4 +1,67 @@
-// { Driver Code Starts
+// Method 1
+class Solution
+{
+	public:
+	//Function to find number of strongly connected components in the graph.
+	void dfs(int n,stack<int> &st,vector<int> &vis, vector<int> adj[])
+	{
+	    vis[n]=1;
+	    for(auto x:adj[n])
+	    {
+	        if(!vis[x])
+	        {
+	            dfs(x,st,vis,adj);
+	        }
+	    }
+	    st.push(n);
+	}
+	void redfs(int node, vector<int> &vis,vector<int> trans[])
+	{
+	    vis[node]=1;
+	    for(auto x:trans[node])
+	    {
+	        if(!vis[x])
+	        {
+	            redfs(x,vis,trans);
+	        }
+	    }
+	}
+    int kosaraju(int V, vector<int> adj[])
+    {
+        vector<int> vis(V,0);
+        stack<int> st;
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            {
+                dfs(i,st,vis,adj);
+            }
+        }
+        vector<int> trans[V];
+        for(int i=0;i<V;i++)
+        {
+            vis[i]=0;
+            for(auto x:adj[i])
+            {
+                trans[x].push_back(i);
+            }
+        }
+        int count=0;
+        while(!st.empty())
+        {
+            int node=st.top();
+            st.pop();
+            if(!vis[node])
+            {
+                redfs(node,vis,trans);
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+//Method 2
 #include<bits/stdc++.h>
 using namespace std;
 
