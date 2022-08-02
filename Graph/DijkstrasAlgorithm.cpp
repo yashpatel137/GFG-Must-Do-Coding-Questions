c44fd0f61f726f96392e254670fc6707307f57df
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-08-02 09:56:15 +0530

    Update DijkstrasAlgorithm.cpp

--- Graph/DijkstrasAlgorithm.cpp
+++ Graph/DijkstrasAlgorithm.cpp
@@ -1,4 +1,36 @@
-// { Driver Code Starts
+//Method 1
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int>ans(V,INT_MAX);
+        ans[S]=0;
+        pq.push({0,S});
+        while(!pq.empty())
+        {
+            int dist=pq.top().first;
+            int prev=pq.top().second;
+            pq.pop();
+            for(auto &it:adj[prev])
+            {
+                int next=it[0];
+                int nextdist=it[1];
+                if(ans[next]>ans[prev]+nextdist)
+                {
+                    ans[next]=ans[prev]+nextdist;
+                    pq.push({ans[next],next});
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//Method 2
 #include<bits/stdc++.h>
 using namespace std;
 
